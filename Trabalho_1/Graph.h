b8648ee2ba3faf475a8f11433e01c261becf2910
Antonio Drumond <adcdstom@gmail.com>
2025-08-15 19:30:04 -0300

    vert neighbors, remove edge, check edge

--- Trabalho_1/Graph.h
+++ Trabalho_1/Graph.h
@@ -16,12 +16,23 @@ struct Graph{
         if(this->arr) delete[] this->arr;
     }
 
+    void all_verts(){
+        while(add_vert()); // Adicionar todos os vertices possiveis
+    }
+
     bool add_vert(){
         if(last_vert < n){
             last_vert++;
             return true;
         }
         else return false;
+    }
+
+    std::vector<int> vert_neighbors(int vert){
+        if(vert <= last_vert){
+            return arr[vert];
+        }
+        else return {};
     }
 
     bool add_edge(int vert1, int vert2){
@@ -36,8 +47,40 @@ struct Graph{
         else return false; // Vertices invalidos
     }
 
+    bool check_edge(int vert1, int vert2){
+        if(vert1 <= last_vert && vert2 <= last_vert){
+            if(-1 != searchVec(arr[vert1], vert2) && -1 != searchVec(arr[vert2], vert1)){ // Verifica se a aresta existe
+                return true;
+            }
+            else return false; // Vertices validos, aresta inexistente
+        }
+        else return false; // Vertices invalidos
+    }
+
+    bool remove_edge(int vert1, int vert2){
+        if(check_edge(vert1, vert2)){
+            int reps = arr[vert1].size();
+            for(int i=0; i<reps; i++){
+                if(arr[vert1].at(i) == vert2){
+                    arr[vert1].erase(arr[vert1].begin() + i);
+                    i = reps;
+                }
+            }
+            reps = arr[vert2].size();
+            for(int i=0; i<reps; i++){
+                if(arr[vert2].at(i) == vert1){
+                    arr[vert2].erase(arr[vert2].begin() + i);
+                    i = reps;
+                }
+            }
+            return true;
+        }
+        else return false;
+    }
+
     void test(){
-        std::cout << "renato\n"; 
+        std::cout << "n = " << this->n << "\n";
+        std::cout << "last_vert = " << this->last_vert << "\n";
     }
 
     private:
--- Trabalho_1/test.cc
+++ Trabalho_1/test.cc
@@ -3,6 +3,8 @@
 int main (int argc, char *argv[]) {
     Graph* g = new Graph(10);
     g->test();
+    g->all_verts();
+    g->test();
     delete g;
     return 0;
 }
